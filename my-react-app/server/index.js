16425c1723e84a1373b9019d64062c3080cc5553
Slinky <lroberts28@murraystate.edu>
2024-11-20 19:47:13 -0600

    logout and dashboard

--- my-react-app/server/index.js
+++ my-react-app/server/index.js
@@ -185,6 +185,24 @@ app.delete('/api/pets/:id', authenticateToken, (req, res) => {
   });
 });
 
+app.get('/api/user', (req, res) => {
+  const { email } = req.query;
+  // Fetch user by email from the database
+  // Respond with user data or an error if not found
+  User.findOne({ email: email })
+    .then(user => {
+      if (user) {
+        res.json({ fullName: `${user.firstName} ${user.lastName}` });
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Server error', error: err });
+    });
+});
+
+
 // Start the server
 const PORT = 5600;
 app.listen(PORT, () => {
